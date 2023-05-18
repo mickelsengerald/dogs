6eff3392f7d94d0b15d37d31d2642fb31187ea4c
GMickelsen <mickelsengerald@gmail.com>
2023-05-18 18:23:19 -0400

    todomenoscss

--- client/src/components/dogCard/DogList.jsx
+++ client/src/components/dogCard/DogList.jsx
@@ -1,89 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DogCard from './DogCard';
 
 const DogList = ({ dogs }) => {
-  const [orderedDogs, setOrderedDogs] = useState([]);
-  const [order, setOrder] = useState('');
-
-  useEffect(() => {
-    let orderedDogsCopy = [...dogs];
-
-    switch (order) {
-      case 'alph_asc':
-        orderedDogsCopy.sort((a, b) => a.name.localeCompare(b.name));
-        break;
-      case 'alph_desc':
-        orderedDogsCopy.sort((a, b) => b.name.localeCompare(a.name));
-        break;
-        case 'weight_asc':
-          orderedDogsCopy.sort((a, b) => {
-            const weightA = a.weight?.metric || (a.min_weight + a.max_weight) / 2 || 0;
-            const weightB = b.weight?.metric || (b.min_weight + b.max_weight) / 2 || 0;
-            return parseFloat(weightA) - parseFloat(weightB);
-          });
-          break;
-        case 'weight_desc':
-          orderedDogsCopy.sort((a, b) => {
-            const weightA = a.weight?.metric || (a.min_weight + a.max_weight) / 2 || 0;
-            const weightB = b.weight?.metric || (b.min_weight + b.max_weight) / 2 || 0;
-            return parseFloat(weightB) - parseFloat(weightA);
-          });
-          break;
-        case 'height_asc':
-          orderedDogsCopy.sort((a, b) => {
-            const heightA = a.height?.metric || (a.min_height + a.max_height) / 2 || 0;
-            const heightB = b.height?.metric || (b.min_height + b.max_height) / 2 || 0;
-            return parseFloat(heightA) - parseFloat(heightB);
-          });
-          break;
-        case 'height_desc':
-          orderedDogsCopy.sort((a, b) => {
-            const heightA = a.height?.metric || (a.min_height + a.max_height) / 2 || 0;
-            const heightB = b.height?.metric || (b.min_height + b.max_height) / 2 || 0;
-            return parseFloat(heightB) - parseFloat(heightA);
-          });
-          break;
-      case 'life_asc':
-        orderedDogsCopy.sort((a, b) => {
-          const lifeSpanA = parseFloat(a.life_span) || 0;
-          const lifeSpanB = parseFloat(b.life_span) || 0;
-          return lifeSpanA - lifeSpanB;
-        });
-        break;
-      case 'life_desc':
-        orderedDogsCopy.sort((a, b) => {
-          const lifeSpanA = parseFloat(a.life_span) || 0;
-          const lifeSpanB = parseFloat(b.life_span) || 0;
-          return lifeSpanB - lifeSpanA;
-        });
-        break;
-      default:
-        break;
-    }
-
-    setOrderedDogs(orderedDogsCopy);
-  }, [order, dogs]);
-
-  const handleOrderChange = (e) => {
-    setOrder(e.target.value);
-  };
-
-  const displayedDogs = order ? orderedDogs : dogs;
-
   return (
     <div>
-      <select value={order} onChange={handleOrderChange}>
-        <option value="">No filter</option>
-        <option value="alph_asc">Alphabetical A-Z</option>
-        <option value="alph_desc">Alphabetical Z-A</option>
-        <option value="weight_asc">Weight Ascending</option>
-        <option value="weight_desc">Weight Descending</option>
-        <option value="height_asc">Height Ascending</option>
-        <option value="height_desc">Height Descending</option>
-        <option value="life_asc">Life Span Ascending</option>
-        <option value="life_desc">Life Span Descending</option>
-      </select>
-      {displayedDogs.map((dog) => (
+      {dogs.map((dog) => (
         <DogCard key={dog.id} dog={dog} />
       ))}
     </div>
@@ -91,6 +12,7 @@ const DogList = ({ dogs }) => {
 };
 
 export default DogList;
+
 
 
 
--- client/src/components/homePage/HomePage.jsx
+++ client/src/components/homePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
-import { getAllDogs, searchDogsByName } from '../../redux/actions';
+import { getAllDogs, searchDogsByName, getTemperaments } from '../../redux/actions';
 import DogList from '../dogCard/DogList';
 import SearchBar from '../searchBar/SearchBar';
 import Paginator from '../paginator/Paginator';
@@ -12,10 +12,15 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const { dogs, searchedDogs } = useSelector((state) => state);
   const [isSearching, setIsSearching] = useState(false); 
-  const [currentPage, setCurrentPage] = useState(1); // Agrega el estado para la página actual
+  const [currentPage, setCurrentPage] = useState(1);
+  const [order, setOrder] = useState('');
+  const [orderedDogs, setOrderedDogs] = useState([]);
+  const [selectedTemperaments, setSelectedTemperaments] = useState([]);
+
 
   useEffect(() => {
     dispatch(getAllDogs());
+    dispatch(getTemperaments());
   }, [dispatch]);
 
   const handleSearch = (name) => {
@@ -23,24 +28,146 @@ const HomePage = () => {
     dispatch(searchDogsByName(name));
   };
 
+  const handleOrderChange = (e) => {
+    setOrder(e.target.value);
+  };
+
+  const handleTemperamentChange = (temperament) => {
+    setSelectedTemperaments(prevTemperaments => 
+        prevTemperaments.includes(temperament) 
+        ? prevTemperaments.filter(temp => temp !== temperament) 
+        : [...prevTemperaments, temperament]
+    );
+  };
+
+  const { temperaments } = useSelector((state) => state);
+
+  useEffect(() => {
+    // Copia el código de ordenación de DogList aquí
+    let orderedDogsCopy = [...dogs];
+    switch (order) {
+      case 'alph_asc':
+        orderedDogsCopy.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'alph_desc':
+        orderedDogsCopy.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+        case 'weight_asc':
+          orderedDogsCopy.sort((a, b) => {
+            const weightA = a.weight?.metric || (a.min_weight + a.max_weight) / 2 || 0;
+            const weightB = b.weight?.metric || (b.min_weight + b.max_weight) / 2 || 0;
+            return parseFloat(weightA) - parseFloat(weightB);
+          });
+          break;
+        case 'weight_desc':
+          orderedDogsCopy.sort((a, b) => {
+            const weightA = a.weight?.metric || (a.min_weight + a.max_weight) / 2 || 0;
+            const weightB = b.weight?.metric || (b.min_weight + b.max_weight) / 2 || 0;
+            return parseFloat(weightB) - parseFloat(weightA);
+          });
+          break;
+        case 'height_asc':
+          orderedDogsCopy.sort((a, b) => {
+            const heightA = a.height?.metric || (a.min_height + a.max_height) / 2 || 0;
+            const heightB = b.height?.metric || (b.min_height + b.max_height) / 2 || 0;
+            return parseFloat(heightA) - parseFloat(heightB);
+          });
+          break;
+        case 'height_desc':
+          orderedDogsCopy.sort((a, b) => {
+            const heightA = a.height?.metric || (a.min_height + a.max_height) / 2 || 0;
+            const heightB = b.height?.metric || (b.min_height + b.max_height) / 2 || 0;
+            return parseFloat(heightB) - parseFloat(heightA);
+          });
+          break;
+      case 'life_asc':
+        orderedDogsCopy.sort((a, b) => {
+          const lifeSpanA = parseFloat(a.life_span) || 0;
+          const lifeSpanB = parseFloat(b.life_span) || 0;
+          return lifeSpanA - lifeSpanB;
+        });
+        break;
+      case 'life_desc':
+        orderedDogsCopy.sort((a, b) => {
+          const lifeSpanA = parseFloat(a.life_span) || 0;
+          const lifeSpanB = parseFloat(b.life_span) || 0;
+          return lifeSpanB - lifeSpanA;
+        });
+        break;
+      default:
+        break;
+    }
+    setOrderedDogs(orderedDogsCopy);
+  }, [order, dogs]);
+
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  const dogsToDisplay = isSearching ? searchedDogs : orderedDogs; // Usa orderedDogs en lugar de dogs
+
+  
+  const filteredDogs = () => {
+    return orderedDogs.filter((dog) => {
+      if (selectedTemperaments.length === 0) {
+        return true;
+      }
+  
+      let dogTemperaments = [];
+      if (Array.isArray(dog.temperament)) {
+        dogTemperaments = dog.temperament;
+      } else if (typeof dog.temperament === 'string') {
+        dogTemperaments = dog.temperament.split(', ');
+      }
+      return selectedTemperaments.some((temperament) =>
+        dogTemperaments.includes(temperament)
+      );
+    });
+  };
+  
+  const filteredsDogs = filteredDogs();
   // Determina los perros que se deben mostrar en la página actual
-  const dogsToDisplay = isSearching ? searchedDogs : dogs;
+  
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const selectedDogs = dogsToDisplay.slice(startIndex, startIndex + itemsPerPage);
+  const selectedDogs = filteredsDogs.slice(startIndex, startIndex + itemsPerPage);
+  
 
   return (
     <div>
       <SearchBar handleSearch={handleSearch} />
+
+      <select value={order} onChange={handleOrderChange}>
+      <option value="">No filter</option>
+        <option value="alph_asc">Alphabetical A-Z</option>
+        <option value="alph_desc">Alphabetical Z-A</option>
+        <option value="weight_asc">Weight Ascending</option>
+        <option value="weight_desc">Weight Descending</option>
+        <option value="height_asc">Height Ascending</option>
+        <option value="height_desc">Height Descending</option>
+        <option value="life_asc">Life Span Ascending</option>
+        <option value="life_desc">Life Span Descending</option>
+      </select>
+      {temperaments.map((temperament) => (
+    <div key={temperament}>
+        <input 
+            type="checkbox" 
+            id={temperament} 
+            value={temperament} 
+            checked={selectedTemperaments.includes(temperament)} 
+            onChange={() => handleTemperamentChange(temperament)}
+        />
+        <label htmlFor={temperament}>{temperament}</label>
+    </div>
+))}
+
       <DogList dogs={selectedDogs} />
+      
       <Paginator 
         totalItems={dogsToDisplay.length} 
         itemsPerPage={itemsPerPage} 
         onPageChange={handlePageChange}
       />
+
       <Link to="/form">
         <button>Create Dog</button>
       </Link>
@@ -49,6 +176,7 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
 
 
 
